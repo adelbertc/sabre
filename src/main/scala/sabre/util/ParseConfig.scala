de0e06f9734a55a0382119a609af81c95b78bb90
Adelbert Chang <adelbertc@gmail.com>
2013-01-08 11:39:06 -0800

    require config file that specifies # of works is >= 0

--- src/main/scala/sabre/util/ParseConfig.scala
+++ src/main/scala/sabre/util/ParseConfig.scala
@@ -10,7 +10,9 @@ object ParseConfig {
     val masterServerAddr = config(1).split("\\s+")(0)
     val s2t = config.tail.foldLeft(Map.empty[String, Option[Int]]) { (m, line) =>
       val linesplit = line.split("\\s+")
-      if (linesplit.size == 2) m + (linesplit(0) -> Some(linesplit(1).toInt))
+      val threads = linesplit(1).toInt
+      require(threads >= 0)
+      if (linesplit.size == 2) m + (linesplit(0) -> Some(threads))
       else m + (linesplit(0) -> None)
     }
     (edgelist, masterServerAddr, s2t)
