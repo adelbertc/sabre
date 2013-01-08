446eb86ecf878a2eea0ec62b4d099634b2783883
Adelbert Chang <adelbertc@gmail.com>
2013-01-08 11:53:05 -0800

    fixed bug from last commit for indexing out of bounds in config parsing

--- src/main/scala/sabre/util/ParseConfig.scala
+++ src/main/scala/sabre/util/ParseConfig.scala
@@ -10,10 +10,11 @@ object ParseConfig {
     val masterServerAddr = config(1).split("\\s+")(0)
     val s2t = config.tail.foldLeft(Map.empty[String, Option[Int]]) { (m, line) =>
       val linesplit = line.split("\\s+")
-      val threads = linesplit(1).toInt
-      require(threads >= 0)
-      if (linesplit.size == 2) m + (linesplit(0) -> Some(threads))
-      else m + (linesplit(0) -> None)
+      if (linesplit.size == 2) {
+        val threads = linesplit(1).toInt
+        require(threads >= 0)
+        m + (linesplit(0) -> Some(threads))
+      } else m + (linesplit(0) -> None)
     }
     (edgelist, masterServerAddr, s2t)
   }
